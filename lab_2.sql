9a3ef9942f6aacdce2aec3ffbc115d953c364a45
mariaklochkovaa <128250753+mariaklochkovaa@users.noreply.github.com>
2023-09-12 10:05:28 +0300

    Add files via upload

---
+++ lab_2.sql
@@ -0,0 +1,20 @@
+SELECT name, population 
+FROM cities
+WHERE population>=1000000;
+SELECT name, region, population
+FROM cities
+WHERE region IN('E','W')
+ORDER BY population DESC;
+SELECT name, region, population
+FROM cities
+WHERE population>50000 AND region IN('S','C','N');
+SELECT name, region, population
+FROM cities
+WHERE 150000<population<350000 AND region IN('E','W','N')
+ORDER BY name ASC
+LIMIT 20;
+SELECT name, region, population
+FROM cities
+WHERE NOT region IN('E','W')
+ORDER BY population ASC
+LIMIT 10 OFFSET 10; 
