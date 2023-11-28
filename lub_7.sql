a51a2a3ad3be1db8be1bbe702f5a322a55cc9571
mariaklochkovaa <128250753+mariaklochkovaa@users.noreply.github.com>
2023-11-28 07:07:11 +0200

    Update lub_7.sql

--- lub_7.sql
+++ lub_7.sql
@@ -8,14 +8,14 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 DROP TABLE IF EXISTS `categories`;
 CREATE TABLE `categories` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `news`;
 CREATE TABLE `news` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `title` varchar(225) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `title` varchar(225) NOT NULL,
   `category_id` int(11) unsigned NOT NULL,
   `content` text NOT NULL,
   PRIMARY KEY (`id`)
@@ -26,20 +26,21 @@ DROP TABLE IF EXISTS `comments`;
 CREATE TABLE `comments` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `news_id` int(11) unsigned NOT NULL,
-  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `title` varchar(255) NOT NULL,
   `text` text NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 
 
 DROP TABLE IF EXISTS `reitings`;
 CREATE TABLE `reitings` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `news_id` int(11) unsigned NOT NULL,
-  `ip` varchar(15) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `ip` varchar(15) NOT NULL,
   `value` int(5) unsigned NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
--- 2023-11-28 04:39:13
+-- 2023-11-28 05:05:57
